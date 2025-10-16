e19a5d9c31235210e9d1b0ba275d0238479073e0
PeterSteve <pedropote677@gmail.com>
2025-10-16 11:43:16 -0300

    Correct letter counting logic in array.c
    Fix logic for counting letters in the input name.

---
+++ array.c
@@ -0,0 +1,16 @@
+int main () {
+char nome[50];
+printf("digite seu nome", nome);
+scanf(%c", &nome);
+
+  int i;
+  int contador; 
+
+for (i = 0; i < nome[1] != '\0'; ++i) {
+(( nome[i] >= 'A' && nome[i] <= 'Z')) || ((  nome[i] >= 'a' &&  <= nome[i] <= 'z')) {
+  contador++
+ }
+  printf("Numero de letras %d\n", contador);
+}
+
+}
